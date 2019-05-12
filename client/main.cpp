7bbe405e1a35786de9a7feb590f2c8c51033822f
Sabaev <pavelsabay@gmail.com>
2019-05-12 16:03:13 +0300

    fix: client buffer

--- client/main.cpp
+++ client/main.cpp
@@ -11,6 +11,8 @@ static const std::string count = "count";
 static std::string ls = "ls";
 static std::string os = "os";
 static const int server_port = 8080;
+static const std::string server_ip  = "192.168.1.9";
+
 void checkInput(int argc, char *argv[]) {
     if (argc == 3) {
         if (strncmp(argv[1], ls.c_str(), ls.size()) != 0 && strncmp(argv[1], count.c_str(), count.size()) != 0) {
@@ -56,18 +58,18 @@ int main(int argc, char **argv) {
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(8080);
-    serv_addr.sin_addr.s_addr = inet_addr("192.168.56.1");
+    serv_addr.sin_addr.s_addr = inet_addr(server_ip.c_str());
 
     if (argc == 2) {
         sendCommand(argv[1], sock, serv_addr);
     } else {
         sendCommand(argv[1], sock,serv_addr, argv[2]);
     }
-    // std::cerr << os.size() << std::endl;
-    // sendto(sock, os.c_str(), os.size()+1, 0,(struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
     perror("send");
     recv(sock, buf, sizeof(buf), 0);
-    printf("%s", buf);
+    std::cout <<  buf << std::endl;
+
     close(sock);
     return 0;
 }
